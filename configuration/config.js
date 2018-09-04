2975a103fbc688e29436f5eaa902c76c0c59d206
vaibhavz2402 <40337322+vaibhavz2402@users.noreply.github.com>
2018-09-05 04:37:10 +0530

    Update config.js

--- configuration/config.js
+++ configuration/config.js
@@ -4,12 +4,12 @@ module.exports={
 * It consists of Twitter app information, database information.
 */
 
-    "linkedin_api_key" 		: 			"818vmyjz4oe11f",
-    "linkedin_api_secret"	:				"BBV1oqFSPKYkfUi6",
+    "linkedin_api_key" 		: 			"linkedin_api_key",
+    "linkedin_api_secret"	:				"linkedin_api_secret",
     "linkedin_callback_url"				:				"http://localhost:1111/auth/linkedin/callback",
 
-    "facebook_api_key" 		: 			"168824083963534",
-	"facebook_api_secret"	:				"2318a93b28d5769e203a99c8f84eb5fc",
+    "facebook_api_key" 		: 			"facebook_api_key",
+	"facebook_api_secret"	:				"facebook_api_secret",
 	"facebook_callback_url"				:				"http://localhost:1111/auth/facebook/callback",
 	"use_database"				:				"false",
 	"host"								:				"localhost",
