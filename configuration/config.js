22c03ad87853f9d02acd4527de438dfcfc2f6010
vaibhavz2402 <40337322+vaibhavz2402@users.noreply.github.com>
2018-09-05 04:27:49 +0530

    Add files via upload

---
+++ configuration/config.js
@@ -0,0 +1,19 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+    "linkedin_api_key" 		: 			"818vmyjz4oe11f",
+    "linkedin_api_secret"	:				"BBV1oqFSPKYkfUi6",
+    "linkedin_callback_url"				:				"http://localhost:1111/auth/linkedin/callback",
+
+    "facebook_api_key" 		: 			"168824083963534",
+	"facebook_api_secret"	:				"2318a93b28d5769e203a99c8f84eb5fc",
+	"facebook_callback_url"				:				"http://localhost:1111/auth/facebook/callback",
+	"use_database"				:				"false",
+	"host"								:				"localhost",
+	"username"						:				"root",
+	"password"						:				"",
+	"database"						:				""
+}
